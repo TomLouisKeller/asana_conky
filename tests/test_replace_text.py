fcdc6975de22fe73580e1824e7b44e364cc3bbe5
Tom Keller <tomlouiskeller@protonmail.com>
2020-11-03 19:47:08 +0100

    Fix replace_text_in_file test

--- tests/test_replace_text.py
+++ tests/test_replace_text.py
@@ -1,5 +1,5 @@
 from asana_conky.configuration import Configuration
-from asana_conky.helper import replace_text
+from asana_conky.helper import replace_text_in_file
 
 
 class TestReplaceText:
@@ -9,17 +9,23 @@ class TestReplaceText:
     def setup_class(cls):
         cls.config = Configuration()
 
-    # def setup_method(self, method):
-    #     self.data = pd.read_csv("tests/resources/market_data.csv")
-    #     self.logger.debug(f"Testing Market Data: \n{self.data}\n")
-    #     self.env = MarketEnvironment(self.logger, self.data)
-    #     self.env.reset()
+    # helper
+    @staticmethod
+    def read_file(file_path: str):
+        with open(file_path, 'r') as file:
+            filedata = file.read()
+        return filedata
 
-    # test init
+    # test replace_text_in_file
+    def test_replace_text_in_file(self):
+        path = 'tests/resources/replace_in_here.txt'
+        start_tag = '${start_tag}'
+        end_tag = '${end_tag}'
 
-    def test_replace(self):
-        tagged_tasks = self.config.get('tagged_tasks')
-        line_string = "this is the new text"
-        replace_text(tagged_tasks['output_path'], tagged_tasks['start_tag'], tagged_tasks['end_tag'], line_string)
+        # Reset file
+        replace_text_in_file(path, start_tag, end_tag, "")
+        one = self.read_file(path)
+        replace_text_in_file(path, start_tag, end_tag, "this is the new text")
+        two = self.read_file(path)
 
-        assert True
+        assert one != two
